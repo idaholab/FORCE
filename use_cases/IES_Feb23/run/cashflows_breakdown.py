6f7ef5e5eaf33449001ada5c5d822a8ec6670cab
Marisol Garrouste (INL) <marisol.garrouste@inl.gov>
2023-01-20 13:28:59 -0500

    Total cashflow over project lifetime instead of average

--- use_cases/IES_Feb23/run/cashflows_breakdown.py
+++ use_cases/IES_Feb23/run/cashflows_breakdown.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
+plants = ['braidwood', 'cooper', 'prairie_island', 'stp', 'davis_besse'] # Add houston when converged
+CASHFLOWS = ['h2_storage_capex', 'diesel_sales','jet_fuel_sales','naphtha_sales','e_sales','elec_cap_market','co2_shipping',\
+  'ft_vom','ft_fom','ft_capex','h2_ptc','htse_elec_cap_market','htse_vom','htse_fom','htse_capex'] #add rest later
+
 def get_final_npv(plant):
   opt_out_file = os.path.join(".", plant, "gold","opt_soln_0.csv")
   df = pd.read_csv(opt_out_file)
@@ -56,34 +60,68 @@ def compute_cashflows(final_out):
     for l in lines:
       if ("CashFlow INFO (proj comp): Project Cashflow" in l) and (c in l) and ("amortize" not in l) and ("depreciate" not in l):
         ind = lines.index(l)
-        if "capex" in c:
-          avg_cashflow = float(lines[ind+2].split(" ")[-1])
-          dic[c] = avg_cashflow
-        else:
-          values = []
-          for i in range(3,23):
-            values.append(float(lines[ind+i].split(" ")[-1]))
-          avg_cashflow = sum(values)/len(values)
-          dic[c] = [avg_cashflow]
+        values = []
+        # Get value for each year and sum it up
+        for i in range(3,23):
+          values.append(float(lines[ind+i].split(" ")[-1]))
+        dic[c] = [sum(values)]
   return dic
 
+def plot_cashflows_2(csv_file):
+  result_df = pd.read_csv(csv_file)
+  result_df = result_df.rename(columns={'elec_cap_market':'ft_elec_cap_market'})
+  # Divide by 1e6 for results in M$
+  for c in list(result_df.columns):
+    if 'plant' not in str(c):
+      result_df[c] /=1e6
+  color_mapping ={
+    'jet_fuel_sales':'#FFD700',
+    'diesel_sales':'#8B3E2F',
+    'naphtha_sales':'#FF6103',
+    'h2_ptc':'#B8860B',#darkgoldenrod',
+    'e_sales':'#B22222',#firebrick',
+    'h2_storage_capex':'#98F5FF',#cadetblue1',
+    'htse_capex':'#104E8B',#cadetblue2',
+    'ft_capex':'#7AC5CD',#cadetblue3',
+    'htse_fom':'#7FFF00',#chartreuse1',
+    'htse_vom':'#458B00',#chartreuse4',
+    'ft_fom':'#BF3EFF',#chartreuse3',
+    'ft_vom':'#68228B',#chartreuse4',
+    'htse_elec_cap_market':'#EEEEE0',#ivory2',
+    'ft_elec_cap_market':'#8B8B83'#ivory4'
+  }
+
+  fig, ax = plt.subplots(1, figsize=(16, 8))
+  ax = result_df.plot.bar(stacked=True,\
+    color=[color_mapping.get(x, '#333333') for x in result_df.columns])
+  legend_labels = [" ".join(l.split("_")).upper() for l in list(result_df.columns)]
+  # x ticks
+  plants_names = [" ".join(p.split('_')).upper() for p in plants]
+  ax.set_axisbelow(True)
+  ax.set_ylabel('Revenues and cost M$(2020)')
+  ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
+  plt.xticks(ticks = result_df.index,labels = plants_names, rotation=0)
+  plt.legend(legend_labels, ncol = 1, bbox_to_anchor=([1, 1.05, 0, 0]), frameon = False)
+  plt.show()
 
 
+def create_cashflow_csv():
+  df = pd.DataFrame(columns=['plant']+CASHFLOWS)
+  df.set_index('plant')
 
-plants = ['braidwood', 'cooper', 'prairie_island', 'stp', 'davis_besse'] # Add houston when converged
-CASHFLOWS = ['h2_storage_capex', 'diesel_sales','jet_fuel_sales','naphtha_sales','e_sales','elec_cap_market','co2_shipping',\
-  'ft_vom','ft_fom','ft_capex','h2_ptc','htse_elec_cap_market','htse_vom','htse_fom','htse_capex'] #add rest later
-df = pd.DataFrame(columns=['plant']+CASHFLOWS)
-df.set_index('plant')
+  for plant in plants:
+    final_out = find_final_out(plant)
+    if final_out:
+      dic = compute_cashflows(final_out)
+      dic['plant'] = plant 
+      temp_df = pd.DataFrame.from_dict(dic)
+      df = pd.concat([df,temp_df], axis=0)
+  df.to_csv("./cashflow_breakdown.csv",index=False)
 
-for plant in plants:
-  final_out = find_final_out(plant)
-  if final_out:
-    dic = compute_cashflows(final_out)
-    dic['plant'] = plant 
-    temp_df = pd.DataFrame.from_dict(dic)
-    df = pd.concat([df,temp_df], axis=0)
-df.to_csv("./cashflow_breakdown.csv",index=False)
+if __name__=="__main__":
+  #create_cashflow_csv()
+  dir = os.path.dirname(os.path.abspath(__file__))
+  plot_cashflows_2(dir+"/cashflow_breakdown.csv")
 
 
 
