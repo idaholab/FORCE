48bb92ab335408a5ea1084d0594bfb8d6872e840
Marisol Garrouste (INL) <marisol.garrouste@inl.gov>
2023-01-23 14:13:18 -0500

    Add table with delta npv

--- use_cases/IES_Feb23/run/cashflows_breakdown.py
+++ use_cases/IES_Feb23/run/cashflows_breakdown.py
@@ -73,21 +73,26 @@ def compute_cashflows(dir, plant, final_out):
   dic['baseline_npv'] = baseline_npv
   return dic
 
-def plot_cashflows_2(dir, csv_file):
+def plot_cashflows_3(dir, csv_file):
   result_df = pd.read_csv(csv_file)
   result_df = result_df.rename(columns={'elec_cap_market':'ft_elec_cap_market'})
-  # Divide by 1e6 for results in M$
+  # Divide by 1e9 for results in bn$ and count baseline npv as cost
   for c in list(result_df.columns):
     if 'plant' not in str(c):
       result_df[c] /=1e9
+    if 'baseline' in str(c):
+      result_df[c] *=-1
+  
+  #Compute delta npv
   color_mapping ={
+    'baseline_npv':'#104E8B',#CDC673
     'jet_fuel_sales':'#FFD700',
     'diesel_sales':'#FFAEB9',
     'naphtha_sales':'#FF6103',
     'h2_ptc':'#B8860B',#darkgoldenrod',
     'e_sales':'#B22222',#firebrick',
     'h2_storage_capex':'#98F5FF',#cadetblue1',
-    'htse_capex':'#104E8B',#cadetblue2',
+    'htse_capex':'#CDC673',#cadetblue2',
     'ft_capex':'#7AC5CD',#cadetblue3',
     'htse_fom':'#7FFF00',#chartreuse1',
     'htse_vom':'#458B00',#chartreuse4',
@@ -98,7 +103,60 @@ def plot_cashflows_2(dir, csv_file):
   }
 
   fig = plt.figure(figsize=(80, 50))
+  # Stacked bars for cashflows
   ax = fig.add_subplot(111)
+  ax_bar = result_df.plot.bar(stacked=True,\
+    color=[color_mapping.get(x, '#333333') for x in result_df.columns])
+  legend_labels = [" ".join(l.split("_")).upper() for l in list(result_df.columns)]
+  # x ticks
+  plants_names = [" ".join(p.split('_')).upper() for p in plants]
+  ax.set_axisbelow(True)
+  ax.set_ylabel('Revenues and cost bn$(2020)')
+  ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
+  ax.set_xticklabels(labels = plants_names, rotation=0)
+  # Add annotation to indicate delta npv
+  dnpv_df = pd.DataFrame()
+  dnpv_df['delta_npv'] = result_df.sum(axis=1)  
+  print(dnpv_df)
+  #ax_bar.bar_label(ax.containers(),labels=dnpv_df['delta_npv'],label_type='center')
+  #ax2 = ax.twinx()
+  #ax2.plot(dnpv_df.index, dnpv_df['delta_npv'], 'rD')
+  plt.legend(legend_labels, ncol = 1, bbox_to_anchor=(1.05,1.0), frameon = False, loc="upper left")
+  plt.gcf().set_size_inches(10, 6)
+  plt.tight_layout()
+  plt.savefig(dir+'/cashflow_breakdown.png')
+
+def plot_cashflows_2(dir, csv_file):
+  result_df = pd.read_csv(csv_file)
+  result_df = result_df.rename(columns={'elec_cap_market':'ft_elec_cap_market'})
+  # Divide by 1e9 for results in bn$ and count baseline npv as cost
+  for c in list(result_df.columns):
+    if 'plant' not in str(c):
+      result_df[c] /=1e9
+    if 'baseline' in str(c):
+      result_df[c] *=-1
+  
+  #Compute delta npv
+  color_mapping ={
+    'baseline_npv':'#104E8B',#CDC673
+    'jet_fuel_sales':'#FFD700',
+    'diesel_sales':'#FFAEB9',
+    'naphtha_sales':'#FF6103',
+    'h2_ptc':'#B8860B',#darkgoldenrod',
+    'e_sales':'#B22222',#firebrick',
+    'h2_storage_capex':'#98F5FF',#cadetblue1',
+    'htse_capex':'#CDC673',#cadetblue2',
+    'ft_capex':'#7AC5CD',#cadetblue3',
+    'htse_fom':'#7FFF00',#chartreuse1',
+    'htse_vom':'#458B00',#chartreuse4',
+    'ft_fom':'#BF3EFF',#chartreuse3',
+    'ft_vom':'#68228B',#chartreuse4',
+    'htse_elec_cap_market':'#EEEEE0',#ivory2',
+    'ft_elec_cap_market':'#8B8B83'#ivory4'
+  }
+
+  fig, ax = plt.subplots()
+  # Stacked bars for cashflows
   ax = result_df.plot.bar(stacked=True,\
     color=[color_mapping.get(x, '#333333') for x in result_df.columns])
   legend_labels = [" ".join(l.split("_")).upper() for l in list(result_df.columns)]
@@ -108,11 +166,17 @@ def plot_cashflows_2(dir, csv_file):
   ax.set_ylabel('Revenues and cost bn$(2020)')
   ax.yaxis.grid(color='gray', linestyle='dashed', alpha=0.7)
   ax.set_xticklabels(labels = plants_names, rotation=0)
+  # Add table below to indicate delta npv
+  delta_npvs = list(result_df.sum(axis=1)) 
+  delta_npvs = [str(round(d,3)) for d in delta_npvs]
+  delta_npvs = [delta_npvs]
+  col_labels =[' ' for p in plants_names]
+  plt.table(cellText=delta_npvs,cellLoc='center', loc ='bottom',rowLabels=[r'$\Delta$ NPV (bn\$(2020))'], colLabels=col_labels)
   plt.legend(legend_labels, ncol = 1, bbox_to_anchor=(1.05,1.0), frameon = False, loc="upper left")
-  plt.gcf().set_size_inches(10, 6)
+  plt.gcf().set_size_inches(11, 6)
   plt.tight_layout()
+  plt.subplots_adjust(bottom=0.1)
   plt.savefig(dir+'/cashflow_breakdown.png')
-
 
 def create_cashflow_csv(dir):
   df = pd.DataFrame(columns=['plant']+CASHFLOWS)
@@ -129,8 +193,8 @@ def create_cashflow_csv(dir):
 
 if __name__=="__main__":
   dir = os.path.dirname(os.path.abspath(__file__))
-  create_cashflow_csv(dir)
-  #plot_cashflows_2(dir,dir+"/cashflow_breakdown.csv")
+  #create_cashflow_csv(dir)
+  plot_cashflows_2(dir,dir+"/cashflow_breakdown.csv")
 
 
 
