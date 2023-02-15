23bd3b41484c416da2b28218f9c07e9be133ccfa
Marisol Garrouste (INL) <marisol.garrouste@inl.gov>
2023-02-15 09:18:08 -0500

    Clean up repo

--- use_cases/IES_Feb23/run/dispatch_plots.py
+++ use_cases/IES_Feb23/run/dispatch_plots.py
@@ -3,28 +3,29 @@ from pathlib import Path
 import sys, os
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter # type:ignore
 
 # Matplotlib Global Settings
-plt.rc("figure", figsize=(27,17), titleweight='bold')
+plt.rc("figure", figsize=(18,13), titleweight='bold')
 plt.rc(
     "axes",
 #     titlesize=25,
     titleweight="bold",
-    labelsize=18,
+    labelsize=20,
     axisbelow=True,
     grid=True
 )
 plt.rc("savefig", bbox="tight")
-plt.rc("legend", fontsize=18)
-plt.rc(["xtick", "ytick"], labelsize=16)
+plt.rc("legend", fontsize=20)
+plt.rc(["xtick", "ytick"], labelsize=20)
 
 def hydrogen(df, ax):
+  sns.set_theme(style='whitegrid')
+  sns.set_context("paper", font_scale=1.75)
   var_cols_1 = [
         'Dispatch__htse__production__h2',
-        'Dispatch__h2_storage__charge__h2', 
-        'Dispatch__h2_storage__discharge__h2',
         'Dispatch__ft__production__h2'
   ]
   var_cols_2 =[
@@ -32,10 +33,10 @@ def hydrogen(df, ax):
   ]
   ax1 = ax
   ax1.set_ylabel('Hydrogen Rate (ton/hour)')
-  labels1 = ["HTSE Production","H2 storage charge", "H2 storage discharge", "FT Consumption"]
+  labels1 = ["HTSE Production", "FT Consumption"]
   labels2 = ["H2 storage level"] 
-  colors1 = ["aqua","violet", "black","orangered"]
-  colors2 = ["chartreuse"]
+  colors1 = ["aqua","orangered"]
+  colors2 = ["violet"]
   for var, lab, col in zip(var_cols_1, labels1, colors1):
       df[var] = abs(df[var])/1e3
       df.plot(x='hour', y=var,ax=ax1,marker='+',label=lab, color=col)
@@ -45,37 +46,47 @@ def hydrogen(df, ax):
   for var, lab, col in zip(var_cols_2,labels2, colors2) :
     df[var] = df[var]/1e3
     df.plot(x='hour', y=var, ax=ax2,marker='.',label=lab,color=col)
-  ax2.tick_params(axis='y', labelcolor='chartreuse')
+  ax2.tick_params(axis='y', labelcolor='violet')
   h1, l1, h2, l2 = ax1.get_legend_handles_labels() + ax2.get_legend_handles_labels()
   ax2.grid(False)
   ax.legend(h1, l1, loc='upper left')
   ax2.legend(h2, l2, loc='upper right')
 
+def price(df, ax):
+  sns.set_theme(style='whitegrid')
+  sns.set_context("paper", font_scale=1.75)
+  df.plot(x='hour', y='price', ax =ax, marker='.', label='Price ($/MWh)', color='tab:red')
+  ax.set_ylabel('Price ($/MWh)')
+  ax.legend(loc="upper left")
 
 def electricity(df, ax):
+  sns.set_theme(style='whitegrid')
+  sns.set_context("paper", font_scale=1.75)
   var_cols_1 = [
         'Dispatch__npp__production__electricity',
-        'Dispatch__htse__production__electricity',
-        'Dispatch__electricity_market__production__electricity'
+        'Dispatch__htse__production__electricity'
   ]
   ax.set_ylabel('Electricity (MW)')
-  labels = ["NPP Production", "HTSE Consumption", "Grid Consumption"]
+  labels = ["NPP Production", "HTSE Consumption"]
   for v in var_cols_1:
     df[v] = abs(df[v])
-  colors = ['tab:green', 'tab:grey', "navy"]
+  colors = ['tab:green', 'tab:grey']
   for var, lab, col in zip(var_cols_1, labels, colors):
     df.plot(x='hour',y=var,ax=ax,marker='+',linestyle='dashed',label=lab, color=col)
   ax.yaxis.set_major_formatter(StrMethodFormatter("{x:,.01f}"))
   ax2 = ax.twinx()
-  ax2.set_ylabel('Price ($/MWh)')
-  df.plot(x='hour', y='price', ax=ax2, marker=".",label='Price ($/MWh)', color='tab:red')
-  ax2.tick_params(axis='y', labelcolor='tab:red')
+  ax2.set_ylabel('Electricity (MW)')
+  df['Dispatch__electricity_market__production__electricity'] *=-1
+  df.plot(x='hour', y='Dispatch__electricity_market__production__electricity', ax=ax2, marker=".",label='Grid Consumption', color="navy")
+  ax2.tick_params(axis='y', labelcolor="navy")
   ax2.grid(False)
   h1, l1, h2, l2 = ax.get_legend_handles_labels() + ax2.get_legend_handles_labels()
   ax.legend(h1, l1, loc='upper left')
   ax2.legend(h2, l2, loc='upper right')
 
 def synfuels(df, ax):
+  sns.set_theme(style='whitegrid')
+  sns.set_context("paper", font_scale=1.75)
   var_cols = [
         'Dispatch__ft__production__diesel',
         'Dispatch__ft__production__naphtha',
@@ -118,17 +129,21 @@ def main(path):
       temp_df = temp_df.loc[temp_df['_ROM_Cluster']==cluster]
       temp_df.set_index('hour')
 
-      fig, axes = plt.subplots(nrows=3, sharex=True)
+      fig, axes = plt.subplots(nrows=4, sharex=True)
 
       for ax in axes:
-          ax.set_xticks(np.arange(0, 121, 24))
+          ax.set_xticks(np.arange(0, 24, 1))
+          #ax.set_xticks(np.arange(0, 121, 24))
           ax.ticklabel_format(axis="y", style="sci", scilimits=(0, 2))
       axes[-1].set_xlabel("Time (h)")
 
       # plot dispatch for different resources on separate subplots
-      electricity(temp_df, axes[0])
-      hydrogen(temp_df, axes[1])
-      synfuels(df, axes[2])
+      sns.set_theme(style='whitegrid')
+      sns.set_context("paper", font_scale=1.85)
+      price(temp_df,axes[0])
+      electricity(temp_df, axes[1])
+      hydrogen(temp_df, axes[2])
+      synfuels(df, axes[3])
       fig.tight_layout()
       fig.savefig(os.path.join('../',results_dir,'dispatch_'+str(cluster)+'.png'))
 
--- use_cases/IES_Feb23/run/functions.py
+++ use_cases/IES_Feb23/run/functions.py
@@ -3,18 +3,6 @@ import pandas as pd
 import os, copy
 import numpy as np
 
-coefs = { 'NOAK':{'m':1.294,
-                  'f':1.382,
-                  'a_sca':862.9, 
-                  'n_sca':-0.501,
-                  'a_mod':365.6,
-                  'n_mod':0},
-          'FOAK':{'m':1.294,
-                  'f':1.382,
-                  'a_sca':862.9, 
-                  'n_sca':-0.501,
-                  'a_mod':365.6,
-                  'n_mod':0}}
 AC_to_DC = 1/1.076 # AC power consumed over DC power consumed
 
 # Conversion coefficients for liquid fuel products
@@ -51,107 +39,6 @@ def h2_ptc(data, meta):
   data = {'reference_price':final_ptc}
   # driver is electricity: 25.13 kg-H2/MWe
   return data, meta
-
-def diesel_credit(data, meta):
-  """
-  Determines the credits for diesel production: 
-  credit only applicable for the first 10 years of the simulation
-  $1.67/gal: $1.25/gal base credit plus $0.01/gal additional credit 
-  for each percentage point reduction in the LCA carbon emissions 
-  above 50% attributed to the synfuel; the ANL indicates a 92% carbon reduction 
-  so $0.42/gal additional credit added to the base $1.25/gal
-  """
-  # Negative diesel activity for diesel market so credit should be negative to result in profit
-  year = meta['HERON']['active_index']['year'] # 0 to 29
-  credit = 0
-  if year<10:
-    credit = 1.67 #$/gal
-  # Diesel activity in kg
-  # Conversion
-  credit = -credit*(1/GAL_to_L)*(1/FUEL_DENSITY['diesel']) #$/kg
-  data = {'reference_price':credit}
-  return data, meta
-
-def jet_fuel_credit(data, meta):
-  """
-  Determines the credits for jet fuel production: 
-  credit only applicable for the first 10 years of the simulation
-  $1.67/gal: $1.25/gal base credit plus $0.01/gal additional credit 
-  for each percentage point reduction in the LCA carbon emissions 
-  above 50% attributed to the synfuel; the ANL indicates a 92% carbon reduction 
-  so $0.42/gal additional credit added to the base $1.25/gal
-  """
-  # Negative diesel activity for diesel market so credit should be negative to result in profit
-  year = meta['HERON']['active_index']['year'] # 0 to 29
-  credit = 0
-  if year<10:
-    credit = 1.67 #$/gal
-  # Diesel activity in kg
-  # Conversion
-  credit = -credit*(1/GAL_to_L)*(1/FUEL_DENSITY['jet_fuel']) #$/kg
-  data = {'reference_price':credit}
-  return data, meta
-
-def get_syngas_capex(data, meta):
-  """
-    Determines the capex cost of the syngas storage
-    @ In, data, dict, request for data
-    @ In, meta, dict, state information
-    @ Out, data, dict, filled data
-    @ Out, meta, dict, state infomation
-  """
-  capacity_kg = meta['HERON']['RAVEN_vars']['syngas_storage_capacity']
-  density = 0.95 #kg/m3
-  cap = capacity_kg/density
-  capex = cap*62.3+66223
-  data = {'driver':capex}
-  return data, meta
-
-def compute_capex(capacity, m,f,a_sca, n_sca, a_mod, n_mod):
-  """ 
-    Determines the capex of the HTSE plant in $/kW-AC
-    @ In, capacity, float, capacity of the HTSE in MW-AC
-    @ In, m, float, indirect cost multiplier
-    @ In, f, float, installation factor
-    @ In, a_sca, float, scalable equipment cost coefficient
-    @ In, n_sca, float, scalable equipment scaling exponent
-    @ In, a_mod, float, modular equipment cost coefficient
-    @ In, n_mod, float, modular equipment scaling exponent
-    @ Out, capex, float, capex in $/kW-AC
-  """
-  capex = m*f* ( a_sca*math.exp(n_sca*math.log(capacity*AC_to_DC)) + a_mod*math.exp(n_mod*math.log(capacity*AC_to_DC)))
-  return capex
-
-def htse_noak_capex(data, meta):
-  """
-    Determines the Capex cost of the HTSE plant (NOAK) in $/MW-AC
-    @ In, data, dict, request for data
-    @ In, meta, dict, state information
-    @ Out, data, dict, filled data
-    @ In, meta, dict, state information
-  """
-  d = coefs['NOAK']
-  m, f, a_sca, n_sca, a_mod, n_mod = d['m'], d['f'], d['a_sca'], d['n_sca'], d['a_mod'], d['n_mod']
-  cap = math.fabs(meta['HERON']['RAVEN_vars']['htse_capacity']) # HTSE capacity cast as negative number
-  capex = -1000*compute_capex(cap, m, f, a_sca, n_sca, a_mod, n_mod)
-  data = {'driver': capex}
-  return data, meta
-
-def htse_noak_capex_comb(data,meta):
-  """
-    Determines the Capex cost of the HTSE plant (NOAK) in $/MW-AC
-    @ In, data, dict, request for data
-    @ In, meta, dict, state information
-    @ Out, data, dict, filled data
-    @ In, meta, dict, state information
-  """
-  d = coefs['NOAK']
-  m, f, a_sca, n_sca, a_mod, n_mod = d['m'], d['f'], d['a_sca'], d['n_sca'], d['a_mod'], d['n_mod']
-  cap = math.fabs(meta['HERON']['RAVEN_vars']['htse_ft_capacity']) # HTSE capacity cast as negative number
-  capex = -1000*compute_capex(cap, m, f, a_sca, n_sca, a_mod, n_mod)
-  data = {'driver': capex}
-  return data, meta
-
 
 def find_lower_nearest_idx(array, value): 
   idx = 0
@@ -214,75 +101,6 @@ def co2_supply_curve_test(data, meta):
   co2_cost = cost_data[idx]
   data = {'reference_price': -co2_cost*co2_demand_year}
   return data, meta 
-
-def co2_supply_curve_comb(data,meta):
-  """
-    Determines the cost of CO2 as a function of the quantity asked for, 
-    For HTSE and FT combined component cases
-    Based on data from D. Wendt analysis on CO2 feedstock
-    @ In, data, dict, request for data
-    @ In, meta, dict, state information
-    @ Out, data, dict, filled data
-    @ In, meta, dict, state information
-  """
-  co2_cost = 0
-  comp_cap = meta['HERON']['RAVEN_vars']['htse_ft_capacity'] #MWe (negative value)
-  elec_to_h2_rate = 25.13 #kg-H2/MWe
-  h2_to_co2_rate = 6.58/1.06 #kg-Co2/kg-h2
-  comp_cap = np.abs(comp_cap)
-  co2_demand_year = 365*24*comp_cap*elec_to_h2_rate*h2_to_co2_rate #(kg-CO2/year)
-  # Get the data for the NPP
-    # Get the data for the NPP
-  labels = meta['HERON']['Case'].get_labels()
-  location = labels['location']
-  location_path = '../data/'+str(location)+'.csv'
-  df = pd.read_csv(os.path.join(os.path.dirname(__file__), location_path))
-  cost_data = df.iloc[:,-1].to_numpy()
-  co2_demand_data = df.iloc[:,-2].to_numpy()
-  diff = np.absolute(co2_demand_data-co2_demand_year)
-  idx = np.argmin(diff)
-  co2_cost = cost_data[idx]
-  data = {'reference_price': -co2_cost}
-  return data, meta
-
-def co2_cost_low(data, meta):
-  """
-    Determines the yearly cost of the co2 feedstock for the FT process assuming a low price fo $1/ton
-    @ In, data, dict, request for data
-    @ In, meta, dict, state information
-    @ Out, data, dict, filled data
-    @ In, meta, dict, state information
-  """
-  co2_cost = 0
-  comp_cap = meta['HERON']['RAVEN_vars']['htse_ft_capacity'] #MWe (negative value)
-  elec_to_h2_rate = 25.13 #kg-H2/MWe
-  h2_to_co2_rate = 6.58/1.06 #kg-Co2/kg-h2
-  co2_demand_year = 365*24*comp_cap*elec_to_h2_rate*h2_to_co2_rate #(kg-CO2/year)
-  # Low price of $25/ton = $0.025/kg
-  co2_price = 0.001 #$/kg
-  co2_cost = co2_price*co2_demand_year
-  data = {'driver': co2_cost} # Cost so negative value
-  return data, meta
-
-def co2_cost_high(data, meta):
-  """
-    Determines the yearly cost of the co2 feedstock for the FT process assuming a high price fo $75/ton
-    @ In, data, dict, request for data
-    @ In, meta, dict, state information
-    @ Out, data, dict, filled data
-    @ In, meta, dict, state information
-  """
-  co2_cost = 0
-  comp_cap = meta['HERON']['RAVEN_vars']['htse_ft_capacity'] #MWe negative
-  elec_to_h2_rate = 25.13 #kg-H2/MWe
-  h2_to_co2_rate = 6.58/1.06 #kg-Co2/kg-h2
-  co2_demand_year = 365*24*comp_cap*elec_to_h2_rate*h2_to_co2_rate #(kg-CO2/year) neg
-  # High price of $75/ton = $0.075/kg
-  co2_price = 0.075 #$/kg
-  co2_cost = co2_price*co2_demand_year # neg
-  data = {'driver': co2_cost} # Cost so negative value
-  return data, meta
-
 
 def jet_fuel_price(data, meta):
   """
@@ -356,17 +174,6 @@ def diesel_price(data, meta):
   data = {'reference_price': price}
   return data, meta 
 
-def test_capex():
-  d = coefs['NOAK']
-  m, f, a_sca, n_sca, a_mod, n_mod = d['m'], d['f'], d['a_sca'], d['n_sca'], d['a_mod'], d['n_mod']
-  capex = -1000*compute_capex(250, m, f, a_sca, n_sca, a_mod, n_mod)
-  print(capex)
-
-def test_co2_supply_curve():
-  meta = {'HERON':{'activity':{'co2':340000}}}
-  data ={}
-  data, meta = co2_supply_curve(data, meta)
-  print(data['driver'])
 
 if __name__ == "__main__":
   #test_co2_supply_curve()# Works!
