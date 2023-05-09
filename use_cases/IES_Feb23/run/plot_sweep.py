ed17e67d83b4788107982f8ec36ed1b9bdcb30ef
Marisol Garrouste (INL) <marisol.garrouste@inl.gov>
2023-05-09 09:21:50 -0400

    Fix get baseline

--- use_cases/IES_Feb23/run/plot_sweep.py
+++ use_cases/IES_Feb23/run/plot_sweep.py
@@ -72,7 +72,7 @@ def pp_sweep_results(sweep_file, mean_NPV_baseline, std_NPV_baseline):
 
 def get_baseline_NPV(case):
   case_list = case.split("_")
-  if ('ptc' in case_list) or ('om' in case_list) or ('capex' in case_list) or ('synfuels' in case_list) or ('co2' in case_list):
+  if ('ptc' in case_list) or ('om' in case_list) or ('elec' in case_list) or ('capex' in case_list) or ('synfuels' in case_list) or ('co2' in case_list):
     baseline_case = case_list[0]+"_baseline"
   elif len(case_list)>1:
     baseline_case ="_".join(case_list[:-1])+"_baseline"
@@ -92,7 +92,7 @@ def get_baseline_NPV(case):
   return mean_NPV, std_NPV
 
 def main():
-  case = "braidwood_ptc_100"
+  case = "braidwood_elec_1.25"
   dir = os.path.dirname(os.path.abspath(__file__))
   os.chdir(dir)
   print("Current Directory: {}".format(os.getcwd()))
