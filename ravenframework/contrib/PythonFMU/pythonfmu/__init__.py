5d4f44901e36f3fe2a48778f3c5e8e258159d917
Botros N. Hanna <botros.hanna@inl.gov>
2023-11-28 09:33:50 -0700

    borrw utils from raven

--- ravenframework/contrib/PythonFMU/pythonfmu/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._version import __version__
-from .builder import FmuBuilder
-from .enums import Fmi2Causality, Fmi2Initial, Fmi2Variability
-from .fmi2slave import Fmi2Slave
-from .variables import Boolean, Integer, Real, String
-from .default_experiment import DefaultExperiment
--- ravenframework/contrib/PythonFMU/setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup
-
-# See setup.cfg for Python package parameters
-setup()
