a002a6e0dfb595a594dc11121772ba102919f952
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2023-12-19 11:05:48 -0700

    Adding support for a heron command.

--- package/setup.py
+++ package/setup.py
@@ -1,9 +1,12 @@
 import sys
 from cx_Freeze import setup, Executable, build_exe
 
+import HERON.templates.write_inner
+
 build_exe_options = {
-    "packages": ["ravenframework","msgpack","ray","crow_modules","AMSC","sklearn"],
-    "includes": ["ray.thirdparty_files.colorama","ray.autoscaler._private"],
+    "packages": ["ravenframework","msgpack","ray","crow_modules","AMSC","sklearn","pyomo","HERON"],
+    "includes": ["ray.thirdparty_files.colorama","ray.autoscaler._private","pyomo.common.plugins","HERON.templates.template_driver"],
+    "include_files": [(HERON.templates.write_inner.__file__,"lib/HERON/templates/write_inner.py")],
     "include_msvcr": True,
 }
 
@@ -12,6 +15,6 @@ setup(
     name="force",
     version="0.1",
     description="FORCE package",
-    executables=[Executable("raven_framework.py")],
+    executables=[Executable("raven_framework.py"),Executable("heron.py")],
     options={"build_exe": build_exe_options},
 )
