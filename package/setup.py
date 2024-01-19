70d5129d07a148f45bdd18a5e015a3b17e5f8dda
j-bryan <jbryan314@gmail.com>
2024-01-19 11:34:36 -0700

    modifications for mac build

--- package/setup.py
+++ package/setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable, build_exe
 import HERON.templates.write_inner
 
 build_exe_options = {
-    "packages": ["ravenframework","msgpack","ray","crow_modules","AMSC","sklearn","pyomo","HERON"],
+    "packages": ["ravenframework","msgpack","ray","crow_modules","AMSC","sklearn","pyomo","HERON", "pyarrow", "netCDF4", "cftime"],
     "includes": ["ray.thirdparty_files.colorama","ray.autoscaler._private","pyomo.common.plugins","HERON.templates.template_driver"],
     "include_files": [(HERON.templates.write_inner.__file__,"lib/HERON/templates/write_inner.py")],
     "include_msvcr": True,
