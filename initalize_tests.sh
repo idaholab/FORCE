12220d70a4f3a8cd7405c45ec3b79ce60aace911
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2024-04-22 12:46:48 -0600

    Adding script to get ready to run tests.

---
+++ initalize_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone git@github.com:idaholab/raven.git
+git clone git@github.com:idaholab/HERON.git
+
+cd raven
+
+git submodule init plugins/TEAL/
+git submodule update
+
+python scripts/install_plugins.py -s plugins/TEAL
+python scripts/install_plugins.py -s ../HERON/
+
+./scripts/establish_conda_env.sh --install
+./build_raven 
+
+(source ./scripts/establish_conda_env.sh --load && conda install -y -c conda-forge openpyxl)
