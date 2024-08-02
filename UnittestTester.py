16a0c014641b57e15f911319fc772e783813ddfa
Caleb Sitton <caleb.sitton@inl.gov>
2024-08-02 10:26:17 -0600

    Started work on unittest tester

---
+++ UnittestTester.py
@@ -0,0 +1,65 @@
+import os
+from raven.scripts import library_handler
+from raven.scripts.TestHarness.testers import RavenPython
+
+try:
+  import unittest
+  unittest_found = True
+except ModuleNotFoundError or ImportError:
+  unittest_found = False
+
+class UnittestTester(RavenPython):
+  """
+  This class simplifies use of the unittest module for running unit tests through rook.
+  """
+
+  @staticmethod
+  def get_valid_params():
+    """
+      Return a list of valid parameters and their descriptions for this type
+      of test.
+      @ In, None
+      @ Out, params, _ValidParameters, the parameters for this class.
+    """
+    params = RavenPython.get_valid_params()
+    params.add_param('unittest_args', '', "Arguments to the unittest module")
+    params.add_param('test_sequence_to_run', '', "Ordered list of test cases in the input file to run")
+    return params
+  
+  def __init__(self, name, params):
+    """
+      Initializer for the class. Takes a String name and a dictionary params
+      @ In, name, string, name of the test.
+      @ In, params, dictionary, parameters for the class
+      @ Out, None.
+    """
+    RavenPython.__init__(self, name, params)
+    
+  def check_runnable(self):
+    """
+      Checks if this test can be run.
+      @ In, None
+      @ Out, check_runnable, boolean, If True can run this test.
+    """
+    if not unittest_found:
+      self.set_skip('skipped (required unittest module is not found)')
+      return False
+    
+    return RavenPython.check_runnable(self)
+  
+  def get_command(self):
+    """
+      returns the command used by this tester.
+      @ In, None
+      @ Out, get_command, string, command to run.
+    """
+    # If the test command has been specified, use it
+    if (command := self._get_test_command()) is not None:
+      return ' '.join([command, '-m unittest', self.specs["unittest_args"], self.specs["input"]])
+
+    # Otherwise, if the python command has been specified, use it
+    if len(self.specs["python_command"]) == 0:
+      pythonCommand = self._get_python_command()
+    else:
+      pythonCommand = self.specs["python_command"]
+    return pythonCommand+" "+self.specs["input"]
